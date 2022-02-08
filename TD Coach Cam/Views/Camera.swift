8202fe0fcae83284ddb33b01a89278e412601cea
tdcoder17 <57919329+tdcoder17@users.noreply.github.com>
2022-02-08 12:13:26 -0600

    Adding camera

---
+++ TD Coach Cam/Views/Camera.swift
@@ -0,0 +1,69 @@
+//
+//  Camera.swift
+//  TD Coach Cam
+//
+//  Created by Theo Davis on 2/8/22.
+//
+
+import SwiftUI
+
+struct Camera: View {
+    var body: some View {
+        CoachCam()
+    }
+}//End of struct
+
+struct Camera_Previews: PreviewProvider {
+    static var previews: some View {
+        Camera()
+    }
+}//End of struct
+
+struct CoachCam: View {
+
+    @StateObject var camera = CoachCamModel()
+    
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack {
+                
+                Spacer()
+                
+                HStack {
+                    if camera.isTaken {
+                        Button(action: {}, label: {
+                            Text("Record")
+                                .foregroundColor(.blue)
+                                .fontWeight(.semibold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 20)
+                                .background(Color.white)
+                                .clipShape(Capsule())
+                        }
+                        
+                        Spacer()
+                    } else {
+                        Button(action: {camera.isTaken.toggle()}, label: {
+                            ZStack {
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 65, height: 65)
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 2)
+                                    .frame(width: 75, height: 75)
+                            }
+                        })
+                    }
+                }
+                .frame(height: 75)
+            }
+        }
+    }
+}//End of struct
+
+class CoachCamModel: ObservableObject {
+    @Published var isTaken = false
+}//End of class
